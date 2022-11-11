9154ff4650b7dc82931128ed1216073ef0465148
Oscar Morris <twocap06@gmail.com>
2022-11-11 13:21:11 +0000

    added main.c and algorithm.h

---
+++ algorithm.h
@@ -0,0 +1,2 @@
+void displayValue(__uint32_t state, int statelen);
+int algorithm(__uint32_t state, __uint8_t rule, int statelen, int wrap);
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "algorithm.h"
+
+int main()
+{
+	algorithm(1U << 15, 90, 32, 0);
+	return 0;
+}
